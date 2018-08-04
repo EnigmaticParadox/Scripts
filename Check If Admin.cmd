2a88551908f3f7eabcab5489729b141e3903a2bb
EnigmaticParadox <41224452+EnigmaticParadox@users.noreply.github.com>
2018-08-03 21:40:31 -0400

    Creation of Check If Admin script

---
+++ Check If Admin.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+:: Check to see if script is being run with Administrator privileges
+:: Script will exit if not being run as Administrator
+
+:checkForAdmin
+
+	openfiles>nul 2>&1
+
+	if %errorlevel% EQU 0 (
+
+		goto :printMenu
+
+	)
+
+	echo.
+	echo.   You are not running as Administrator.
+	echo.
+	echo.   Please run this from an Elevated Command Prompt
+	echo.   Or Right-Click SetOfficeChannel.cmd and Select "Run as Administrator"
+	echo.
+	echo.   Press any key to exit...
+
+	pause>nul
+
+goto :eof
